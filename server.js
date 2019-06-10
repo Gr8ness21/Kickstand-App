2bc177f974a493824fbb25183a4727248daa9a92
Gr8ness21 <pmleechapman@gmail.com>
2019-06-10 09:45:03 -0400

    connecting react mongo and express to appear in heroku

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+
 app.get('/', (req,res) => {
-  res.send("I'm in dis bih!")
+  res.send(__dirname + "/client/build.index.html")
 })
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Magic happening on port " + PORT);
 })
+
+app.use(express.static(__dirname + '/client/build/'));
