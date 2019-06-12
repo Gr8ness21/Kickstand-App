a0c3039e8d3c3bbbeb6085f703fa3323c8a1ddc2
Gr8ness21 <pmleechapman@gmail.com>
2019-06-12 15:05:01 -0400

    fixed components to render locally

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Cities from "./components/Cities"
 import SingleCity from "./components/SingleCity"
 import Events from "./components/Events"
 import SingleEvent from "./components/SingleEvent"
 import Nav from "./components/Nav"
+import Home from "./components/Home"
 
 
 class App extends Component {
@@ -15,19 +16,25 @@ class App extends Component {
 
     return (
       <Router>
-        <div>
 
-          <div>
-            <Nav />
-          </div>
-          
+        <div>
+          heyyyyy
+               <div>
+            <Nav/>
+            Whats uppp reacttttttttt
+          </div> 
+
+        <Switch>
+
           <Route exact path="/" component={Home} />
           <Route exact path="/cities" component={Cities} />
           <Route exact path="/events" component={Events} />
           <Route exact path="/cities/:id" component={SingleCity} />
           <Route exact path="/singleEvent/:id" component={SingleEvent} />
 
+        </Switch>
         </div>
+
       </Router>
     )
   }
