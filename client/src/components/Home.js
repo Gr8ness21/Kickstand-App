79253e3302619d6001491593242059030bed9c04
Gr8ness21 <pmleechapman@gmail.com>
2019-06-12 12:43:46 -0400

    added home page

---
+++ client/src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+class Home extends Component {
+    render() {
+
+        return (
+
+            <div className="homePage">
+                <h1> Welome to Kickstand Events! </h1>
+                <p> YOUR ONE STOP SHOP FOR BIKER EVENTS IN YOUR AREA!<br/>
+                Check it Out!
+                </p>
+            </div>
+
+        )
+    }
+}
+
+export default Home;
