15da99e8bdfbd9a66940f7669bd1b60cbc42e043
Gr8ness21 <pmleechapman@gmail.com>
2019-06-12 12:33:44 -0400

    created src routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Cities from "./components/Cities"
+import SingleCity from "./components/SingleCity"
+import Events from "./components/Events"
+import SingleEvent from "./components/SingleEvent"
+import Nav from "./components/Nav"
+
+
+class App extends Component {
+  render() {
+
+    return (
+      <Router>
+        <div>
+
+          <div>
+            <Nav />
+          </div>
+          
+          <Route exact path="/" component={Home} />
+          <Route exact path="/cities" component={Cities} />
+          <Route exact path="/events" component={Events} />
+          <Route exact path="/cities/:id" component={SingleCity} />
+          <Route exact path="/singleEvent/:id" component={SingleEvent} />
+
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
